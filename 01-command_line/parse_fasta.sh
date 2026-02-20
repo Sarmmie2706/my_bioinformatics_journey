8d74b590a10db8ca956a92fdc45851ff759083a7
Sarmmie2706 <aladegbaiyesarmmie@gmail.com>
2026-02-20 11:43:49 +0100

    Add lines for comments and tracking progress

--- 01-command_line/parse_fasta.sh
+++ 01-command_line/parse_fasta.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-set -eu
+# Adding file to capture executions
+exec 3> parse_debug.log
+export BASH_XTRACEFD=3
+set -eux
 mkdir organisms
 organism=""
+
+# Read every line in the input file
 while IFS= read -r line
 do
+	# If the line is a header line, set the fasta file to input into
+	# If it isn't, append to the current fasta file 
 	if [[ ${line} == ">"* ]]
 	then
+		# Checking which organism it is to determine which fasta file is used
 		case ${line} in
 		*"OS=Pan"*)
 		organism="chimp"
@@ -39,6 +47,7 @@ do
 		organism="animal"
 		esac
 		echo "${line}" >> "organisms/${organism}.fasta"
+		echo "Done setting new header's fasta file"
 	else
 		echo "${line}" >> "organisms/${organism}.fasta"
 	fi
