ce76929fd18f35ff020bf46113924fa540cec258
Sarmmie2706 <aladegbaiyesarmmie@gmail.com>
2026-02-20 16:29:33 +0100

    Add code for each organism's total sequence count

--- 01-command_line/optimized_parse_fasta.sh
+++ 01-command_line/optimized_parse_fasta.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 
 organism=""
+mkdir organisms
 while IFS= read -r line
 do
 	if [[ ${line} == ">"* ]]
 	then
 		organism=$(echo ${line} | grep -oP "(?<=OS=).*?(?= OX)" | sed "s/ /_/g")
-		if [[ -e "${organism}.fasta" ]]
+		if [[ ! -e "organisms/${organism}.fasta" ]]
 		then
-			echo ${line} >> "${organism}.fasta"
+			org_name=$(echo ${line} | grep -oP "(?<=_).*?(?= )")
+			echo ${line} > "organisms/${organism}.fasta"
 		else
-			echo ${line} > "${organism}.fasta"
+			echo ${line} >> "organisms/${organism}.fasta"
 		fi
 	else
 		echo ${line} >> "${organism}.fasta"
 	fi
 done < uniprot_accession.fasta
+
+cd organisms/
+for file in *.fasta
+do
+	while IFS= read -r file_line
+	do
+		if [[ ${file_line} == ">"* ]]
+		then
+			org_name=$(echo ${file_line} | grep -oP "(?<=_).*?(?= )")
+		fi
+	done < ${file}
+	count=$(less ${file} | grep -c ">")
+        echo "There are ${count} ${org_name} sequences in ${file}"
+done 
+
+cd ..
+rm -r organisms/
