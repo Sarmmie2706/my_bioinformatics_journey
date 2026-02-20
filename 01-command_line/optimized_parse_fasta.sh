1c6e28c72ea37faa19b92a8337f63f0882d23208
Sarmmie2706 <aladegbaiyesarmmie@gmail.com>
2026-02-20 17:11:15 +0100

    Add lines for tracking progress and comments

--- 01-command_line/optimized_parse_fasta.sh
+++ 01-command_line/optimized_parse_fasta.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+exec 3> optimize_parse_debug.log
+export BASH_XTRACEFD=3
+set -eux
+
 organism=""
 mkdir organisms
+
+# Read every line in the input file
 while IFS= read -r line
 do
+	# If the line is a header, extract the organism name to create a file,
+	# else append line into existing file
 	if [[ ${line} == ">"* ]]
 	then
 		organism=$(echo ${line} | grep -oP "(?<=OS=).*?(?= OX)" | sed "s/ /_/g")
+		# If the file exists, append the line into it,
+		# If it doesn't, echo the line into a new file
 		if [[ ! -e "organisms/${organism}.fasta" ]]
 		then
-			org_name=$(echo ${line} | grep -oP "(?<=_).*?(?= )")
 			echo ${line} > "organisms/${organism}.fasta"
 		else
 			echo ${line} >> "organisms/${organism}.fasta"
@@ -19,18 +28,23 @@ do
 	fi
 done < uniprot_accession.fasta
 
+# Change into the created directory
 cd organisms/
+
+# Read every file in the new directory 
 for file in *.fasta
 do
 	while IFS= read -r file_line
 	do
+		# If the file is a header, extract the organism name 
 		if [[ ${file_line} == ">"* ]]
 		then
 			org_name=$(echo ${file_line} | grep -oP "(?<=_).*?(?= )")
 		fi
 	done < ${file}
+	# Count the number of headers in each file
 	count=$(less ${file} | grep -c ">")
-        echo "There are ${count} ${org_name} sequences in ${file}"
+        echo "There are ${count} ${org_name} sequences in the ${file} file"
 done 
 
 cd ..
