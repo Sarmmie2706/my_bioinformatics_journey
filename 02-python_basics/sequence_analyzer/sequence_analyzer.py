065f4b1e2c084886fd9287665ef99fa3f43c21c9
Sarmmie2706 <aladegbaiyesarmmie@gmail.com>
2026-03-10 09:21:40 +0100

    Add reverse complement function

--- 02-python_basics/sequence_analyzer/sequence_analyzer.py
+++ 02-python_basics/sequence_analyzer/sequence_analyzer.py
@@ -2,8 +2,12 @@
 sequence = input("Please enter a valid DNA sequence:")
 bases = ["A", "T", "C", "G"]
 lower_bases = [base.lower() for base in bases]
-guan_cyto = ["C", "G", "c", "g"]
-
+gc_bases = ["C", "G", "c", "g"]
+complement_bases = {
+    "A": "T", "T": "A", "G": "C", "C": "G",
+    "a": "t", "t": "a", "g": "c", "c": "g"
+}
+print(sequence)
 
 # %%
 def is_valid(sequence):
@@ -28,7 +32,7 @@ count_bases(sequence)
 def gc_content(sequence):
     gc_count = 0
     for base in sequence:
-        if base in guan_cyto:
+        if base in gc_bases:
             gc_count += 1
     gc_percent = round((gc_count/len(sequence)) * 100, 2)
     return gc_percent
@@ -36,3 +40,11 @@ gc_content(sequence)
 
 # %%
 def reverse_complement(sequence):
+    complement = ""
+    for base in sequence:
+        complement += complement_bases[base]
+    rev_complement = complement[::-1]
+    return rev_complement
+reverse_complement(sequence)
+
+# %%
