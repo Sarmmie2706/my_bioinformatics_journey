a2eeb89a189ef3b7105c4b8e1325243892d1fc40
Sarmmie2706 <aladegbaiyesarmmie@gmail.com>
2026-03-11 04:11:25 +0100

    Call other function & optimize for lower case input

--- 02-python_basics/sequence_analyzer.py
+++ 02-python_basics/sequence_analyzer.py
@@ -1,30 +1,26 @@
 # %%
-sequence = "gtatagatgcccgctaacgtctaggcagtacggattccagtacac"
 bases = ["A", "T", "C", "G"]
-lower_bases = [base.lower() for base in bases]
-gc_bases = ["C", "G", "c", "g"]
+gc_bases = ["C", "G"]
 complement_bases = {
-    "A": "T", "T": "A", "G": "C", "C": "G",
-    "a": "t", "t": "a", "g": "c", "c": "g"
+    "A": "T", "T": "A", "G": "C", "C": "G"
 }
 motifs = ["ATG", "TAA", "TAG", "TGA"]
-lower_motifs = [motif.lower() for motif in motifs]
+
 
 # %%
 def is_valid(sequence):
     for base in sequence:
-        if base not in bases and base not in lower_bases:
-            print("This is not a valid DNA sequence.")
+        if base not in bases:
             return False
     
     # If all the bases are in ATCG
     return True
-is_valid(sequence)  
+
 
 # %%
 def sequence_properties(sequence):
     sequence_length = len(sequence)
-    print(f"The sequence is {sequence_length} base pairs long\n")
+    print(f"Length: {sequence_length} base pairs.\n")
 
 
 # %%
@@ -36,6 +32,7 @@ def count_bases(sequence):
     for base, percent in count.items():
         print(f"{base}: {percent}")
 
+
 # %%
 def gc_content(sequence):
     gc_count = 0
@@ -44,7 +41,7 @@ def gc_content(sequence):
             gc_count += 1
     gc_percent = round((gc_count/len(sequence)) * 100, 2)
     return gc_percent
-gc_content(sequence)
+
 
 # %%
 def reverse_complement(sequence):
@@ -52,39 +49,48 @@ def reverse_complement(sequence):
     for base in sequence:
         complement += complement_bases[base]
     rev_complement = complement[::-1]
-    return rev_complement
-reverse_complement(sequence)
+    print(f"Reverse Complement: {rev_complement}")
+
 
 # %%
 def find_motif(sequence):
     for i in range(0, len(sequence), 3):
-        if sequence[i:i+3] in motifs or sequence[i:i+3] in lower_motifs:
-            return i+1
-find_motif(sequence)
+        if sequence[i:i+3] in motifs:
+            if sequence[i:i+3] == "ATG":
+                print(f"Start codon found at position {i+1}")
+            else:
+                print(f"Stop codon found at position {i+1}")
 
 # %%
 def analyze_sequence():
-    # sequence = input("Please enter a valid sequence:")
-    sequence = "gtatagatgcccgctaacgtctaggcagtacggattccagtacac"
+    sequence = input("Please enter a valid sequence:")
+    sequence = sequence.upper()
 
-    print("DNA Sequence Analysis\n======================")
+    print("DNA Sequence Analysis\n======================================================")
     print(f"DNA Sequence:{sequence}\n")
     if is_valid(sequence) == True:
-        print("This is a valid DNA sequence.\n")
+        print("Valid DNA sequence.\n")
         sequence_properties(sequence)
         
         print("Base Counts")
         count_bases(sequence)
 
         print("\n")
-        print(f"It's GC content is {gc_content(sequence)}%")
+        print(f"GC content: {gc_content(sequence)}%")
 
+        print("\n")
+        reverse_complement(sequence)
 
+        print("\n")
+        find_motif(sequence)
 
+        print("\n")
+        print("Analysis Complete!")
+
+    else:
+        print("Please enter a valid DNA sequence")
 
 analyze_sequence()
-
-# %%
 
 
 # %%
