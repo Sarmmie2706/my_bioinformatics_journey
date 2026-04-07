f21fe8b0d400f2b737984794d71b9c7a765e606f
Sarmmie2706 <aladegbaiyesarmmie@gmail.com>
2026-04-07 14:40:27 +0100

    Save plots and add comments

--- 03-r_programming/r_final_project/cancer_genomics_report.R
+++ 03-r_programming/r_final_project/cancer_genomics_report.R
@@ -65,19 +65,21 @@ expr_matrix["CDH1",   1:30] <- pmax(expr_matrix["CDH1",  1:30] - 200, 0)
 col_data <- DataFrame(clinical)
 col_data$disease_state <- rep(c("Tumour", "Normal"), each = 30)
 
+# Create annotation table
 anno <- select(
   org.Hs.eg.db,
   keys = gene_names,
   columns = c("ENTREZID","GENENAME", "CHR"),
   keytype = "SYMBOL"
 )
-anno
+
 row_data <- DataFrame(
   gene = gene_names,
   full_name = anno$GENENAME,
   chromosome = anno$CHR
 )
 
+# Build the summarized experiment object
 se = SummarizedExperiment(
   assays = list(counts = expr_matrix),
   colData = col_data,
@@ -116,24 +118,30 @@ mutations <- data.frame(
   stringsAsFactors = FALSE
 )
 
+# Check for NA values
 colSums(is.na(clinical))
 colSums(is.na(mutations))
 
 # Join clinical metadata onto the mutations table so each mutation
 # row also has the patient's age, cancer type, stage, and group
 mutations_metadata <- left_join(mutations, clinical, by = "patient_id")
+
+# Generate each patient's mutation burden, add to the clinical metadata and
+# replace NAs with zero, if any
 mutation_burden <- mutations_metadata |> 
   summarise(
     mut_burden = n(),
     .by = patient_id
   )
-
 clinical <- left_join(clinical, mutation_burden, by = "patient_id")
 
 clinical <- clinical |> 
   mutate(
     mut_burden = replace_na(mut_burden, 0)
   )
+
+# Convert the expression matrix to a dataframe in order to pivot to the
+# long format and then join to the clinical metadata
 expr_df <- as.data.frame(expr_matrix)
 expr_df <- expr_df |> 
   rownames_to_column(var = "genes")
@@ -143,12 +151,13 @@ expr_df_long <- pivot_longer(
   names_to = "patient_id",
   values_to = "expression"
 )
-
 clinical_expr_long <- left_join(expr_df_long, clinical, by = "patient_id")
 
+# Join the long expression data to make a full annotation table
 full_anno_table <- left_join(expr_df_long, anno, by = c("genes" = "SYMBOL"))
 print(full_anno_table)
 
+# Separate the long expression data based on group to check for normality
 tumour_expr <- clinical_expr_long |> 
   filter(group == "Tumour") |> 
   mutate(
@@ -167,14 +176,15 @@ clinical_expr_long <- clinical_expr_long|>
     log2_expr = log2(expression + 1)
   )
 
+# Using Wilcoxon test as data is not normally distributed
 wilcox_p.values <- clinical_expr_long %>%
   group_by(genes) %>%
   summarise(
     p_value = wilcox.test(expression ~ group)$p.value
   )
-
 padj <- p.adjust(wilcox_p.values$p_value, method = "BH")
 
+# Calculate each gene's tumour and normal expression to generate log2FC
 mean_tumour <- tumour_expr |> 
   summarise(
     mean_tumour = round(mean(expression)),
@@ -186,13 +196,17 @@ mean_normal <- normal_expr |>
     .by = genes
   )
 log2FC <- log2(mean_tumour$mean_tumour/mean_normal$mean_tumour)
+
+#Save the important results to a dataframe
 results_df <- data.frame(
   gene = mean_normal$genes,
   log2FC = round(log2FC, 3),
   pvalue = wilcox_p.values$p_value,
   padj = padj
 )
+results_df
 
+#Filter the significant genes
 sig_results_df <- results_df |> 
   filter(padj < 0.05)
 
@@ -203,15 +217,27 @@ mean_df <- data.frame(
   label = paste("Mean =", round(mean_mut_burden, 2))
 )
 
-ggplot(clinical, aes(x = fct_reorder(patient_id, -mut_burden), y = mut_burden, fill = cancer_type)) +
+# Bar chart of mutation  burden per patient
+p1 <- ggplot(clinical, aes(x = fct_reorder(patient_id, -mut_burden), y = mut_burden, fill = cancer_type)) +
   geom_bar(stat = "identity") +
   geom_hline(yintercept = mean_mut_burden,
              linetype = "dashed",
              colour = "red") +
   geom_text(data = mean_df, 
-            aes(, x = x, y = y, label = label),
-            inherit.aes = FALSE)
+            aes(x = x, y = y, label = label),
+            inherit.aes = FALSE) +
+  labs(
+    title = "Mutation Burden in Different Patients",
+    x = "Patients",
+    y = "Mutation Burden"
+  ) +
+  facet_wrap(~ group) +
+  theme(axis.text.x = element_blank(),    
+        axis.ticks.x = element_blank()) 
+ggsave(filename = "fig1_mutation_burden.png", plot = p1,
+       width = 9, height = 5, dpi = 300)
 
+# Add a status to the results dataframe based on log2FC value
 results_df <- results_df |> 
   mutate(
     status = case_when(
@@ -221,30 +247,41 @@ results_df <- results_df |>
     )
   )
 
-ggplot(results_df, aes(log2FC, -log10(pvalue), colour = status)) +
+# Volcano plot showing log2FC and -log10(pvalue)
+p2 <- ggplot(results_df, aes(log2FC, -log10(pvalue), colour = status)) +
   geom_point() +
   geom_text(data = sig_results_df, aes(x = log2FC, y = -log10(pvalue), label = gene),
              inherit.aes = FALSE,
              vjust = -1) +
   geom_hline(yintercept = -log10(0.05), linetype = "dashed", colour = "red") +
-  geom_vline(xintercept = 0, linetype = "dashed", colour = "red")
+  geom_vline(xintercept = 0, linetype = "dashed", colour = "red") +
+  labs(
+    title = "Volcano Plot Showing DEGs with Significant P-Values",
+    x = "Fold Change",
+    y = "P-Values (Negative Logarithm)"
+  )
+ggsave(filename = "fig2_volcano.png", plot = p2,
+       width = 7, height = 7, dpi = 300)
 
-# PCA 
+# Principal Component Analysis
 log2_expr_matrix <- log2(expr_matrix + 1)
 pca <- prcomp(t(log2_expr_matrix), center = TRUE, scale. = TRUE)
 variance_exp <- round(((pca$sdev)^2 / sum((pca$sdev) ^ 2) * 100), 2)
 pca$var_exp <- variance_exp
 
+
 pca_df <- cbind(clinical, pca$x)
 
-ggplot(pca_df, aes(PC1, PC2, colour = group)) +
+# Principal Components Coloured Based on Group
+p3 <- ggplot(pca_df, aes(PC1, PC2, colour = group)) +
   geom_point() +
   labs(
     x = paste0("PC1 Variance explained: ", variance_exp[1], "%"),
     y = paste0("PC2 Variance explained: ", variance_exp[2], "%")
   )
 
-ggplot(pca_df, aes(PC1, PC2, colour = cancer_type)) +
+# PCs Coloured Based on Cancer Type
+p4 <- ggplot(pca_df, aes(PC1, PC2, colour = cancer_type)) +
   geom_point() +
   labs(
     x = paste0("PC1 Variance explained: ", variance_exp[1], "%"),
@@ -254,24 +291,49 @@ ggplot(pca_df, aes(PC1, PC2, colour = cancer_type)) +
 scree_df <- data.frame(
   PC       = 1:length(variance_exp),
   variance = variance_exp
-) 
+)
+
+#Scree plots of first 10 PCs
 scree_df <- scree_df[1:10, ]
-ggplot(scree_df, aes(x = PC, y = variance)) +
+p6 <- ggplot(scree_df, aes(x = PC, y = variance)) +
   geom_bar(stat = "identity", fill = "blue") + 
   geom_line(colour = "red") +
-  geom_point(colour = "red")
+  geom_point(colour = "red") +
+  labs(
+    title = "Variance Explained by First 10 PCs",
+    x = "PCs",
+    y = "Variance Explained (%)"
+  ) +
+  scale_x_continuous(breaks = seq(0, 10, by = 1))
+
+p5 <- (p3 + p4) +
+    plot_layout(
+    axis_titles = "collect",
+    guides = "collect"
+  )
+        
+p7 <- p5 / p6 + plot_annotation(
+  title = "PC1 vs PC2 based on Group and Cancer Type"
+)
+ggsave(filename = "fig3_pca.png", plot = p7,
+       width = 9, height = 7, dpi = 300)
 
 # Survival analysis
+# Create survival object and fit KM curves by group and cancer type
 surv_obj <- Surv(clinical$survival_time, clinical$status)
-surv_obj
-
 km_fit_group <- survfit(surv_obj ~ group, data =  clinical)
 summary(km_fit_group)
-ggsurvplot(km_fit_group,
+
+# KM plot showing  pvalue, confidence intervals and risk table
+p8 <- ggsurvplot(km_fit_group,
            data = clinical,
+           title = "Change in Probability of Survival with Time",
            pval = TRUE,
            conf.int = TRUE,
            risk.table = TRUE)
+g_to_save <- survminer:::.build_ggsurvplot(p8)
+ggsave("fig4_survival.png", g_to_save, width = 10, height = 7, dpi = 300)
+
 survdiff(surv_obj ~ group, data=clinical)
 
 km_fit_cancer <- survfit(surv_obj ~ cancer_type, data =  clinical)
@@ -284,6 +346,7 @@ ggsurvplot(km_fit_cancer,
            risk.table = TRUE)
 survdiff(surv_obj ~ cancer_type, data=clinical)
 
+# Fit a cox proportional hazards model
 cox_model <- coxph(surv_obj ~ group + age + stage + cancer_type,
                    data = clinical)
 summary(cox_model)
@@ -293,16 +356,23 @@ hr_table <- summary(cox_model)$coefficients
 hr_values <- exp(coef(cox_model))  
 pvals <- hr_table[, "Pr(>|z|)"]  # P-values
 
+# Run cox.zph on the cox model
 ph_test <- cox.zph(cox_model)
 ggcoxzph(ph_test)
 
+# Correlation Analysis
 cor_matrix <- cor(t(expr_matrix[, 1:30]), method = "spearman")
-corrplot(cor_matrix,
-         method = "color",
+# Open PNG device to save 
+png("fig5_correlation.png", width = 1200, height = 1200)
+corrplot(cor_matrix, 
+         method = "color", 
          type = "upper",
          addCoef.col = "black",
-         number.cex = 0.7,
-         number.digits = 2)
+         number.cex = 1.5,
+         tl.cex = 1.5)
+title(main = "Cancer Genes Correlation Across 30 Tumour Samples",
+               cex.main = 3.0, line = 1.5)
+dev.off()
 
 # Find strongest correlated gene pair
 spearman_test <- cor.test(expr_matrix["EGFR", 1:30], 
@@ -311,6 +381,7 @@ spearman_test <- cor.test(expr_matrix["EGFR", 1:30],
 print(spearman_test)
 
 # Genomic ranges
+# Create GRanges object
 cancer_genes_gr <- GRanges(
   seqnames = c("chr17","chr17","chr12","chr7","chr8",
                "chr10","chr13","chr5","chr7","chr3",
@@ -358,13 +429,17 @@ somatic_variants <- data.frame(
   stringsAsFactors = FALSE
 )
 
+# Convert somatic variants to GRanges object but give an end position first
 somatic_variants$END <- somatic_variants$POS
-
 somatic_gr <- makeGRangesFromDataFrame(somatic_variants, 
                                keep.extra.columns=TRUE,
                                start.field="POS", 
                                end.field="END")
+
+# Find overlapping genes
 hits <- findOverlaps(somatic_gr, cancer_genes_gr)
+
+# Result table for overlapping genes
 overlaps_df <- data.frame(
   gene_name = cancer_genes_gr$gene[subjectHits(hits)],
   variant_pos = start(somatic_gr)[queryHits(hits)],
@@ -381,6 +456,10 @@ table(overlaps_df$gene)
 # Export overlapping variants as BED file
 overlapping_variants <- somatic_gr[queryHits(hits)]
 
+# Distance of each variant to the nearest cancer gene
+distance_to_nearest <- distanceToNearest(somatic_gr,cancer_genes_gr)
+
+# Write overlapping variants to a bed file
 bed_df <- data.frame(
   chrom  = as.character(seqnames(overlapping_variants)),
   start  = start(overlapping_variants) - 1,  # 0-based
@@ -390,8 +469,6 @@ bed_df <- data.frame(
   strand = "."
 )
 
-distance_to_nearest <- distanceToNearest(somatic_gr,cancer_genes_gr)
-
 write.table(bed_df, "somatic_overlaps.bed",
             sep       = "\t",
             row.names = FALSE,
@@ -399,6 +476,7 @@ write.table(bed_df, "somatic_overlaps.bed",
             quote     = FALSE)
 
 # Logistic Regression
+# Create a binary group to use to fit model
 clinical <- clinical |> 
   mutate(
     group_binary = case_when(group == "Tumour" ~ 1,
@@ -409,10 +487,13 @@ glm_fit <- glm(group_binary ~ age + stage + cancer_type,
                data = clinical)
 summary(glm_fit)
 
+# Extract odds ratio and McFadden's pseudo R
 odds_ratio <- exp(coef(glm_fit))
 mcfadden_r2 <- 1 - (glm_fit$deviance / glm_fit$null.deviance)
-clinical$predicted_prob <- predict(glm_fit, type = "response")
+odds_ratio
 
+# Derive predicted probabilities and plot against age
+clinical$predicted_prob <- predict(glm_fit, type = "response")
 ggplot(clinical, aes(x=age, y=predicted_prob, colour = stage)) +
   geom_line(colour="red", linewidth=1) +
   geom_point(aes(y=group_binary), alpha=0.3) +
@@ -420,8 +501,15 @@ ggplot(clinical, aes(x=age, y=predicted_prob, colour = stage)) +
        x="Age", y="Predicted Probability") +
   theme_bw()
 
-
-
-
+# Final Summary Table
+mutations_per_gene <- mutations |> 
+  summarise(
+    n_mutations = n(),
+    .by = gene
+  )
+results_df <- left_join(results_df, mutations_per_gene, by = "gene")
+results_df <- left_join(results_df, anno, by = c("gene" = "SYMBOL"))
+results_df <- results_df |> 
+  dplyr::select(gene, GENENAME, CHR, ENTREZID, log2FC, pvalue, padj, status, n_mutations)
 
 
