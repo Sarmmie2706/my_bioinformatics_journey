f52145778268d125be443fa91b6073d9ae9b2fb6
Sarmmie2706 <aladegbaiyesarmmie@gmail.com>
2026-04-14 17:20:57 +0100

    Run variant caller and count variants

---
+++ 04-ngs_analysis/03_variants/scripts/variant_pipeline.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/bash
+
+set -eu
+
+# Run the HaplotypeCaller that detects germline mutation. The tp53 region on the chromosome was selected
+gatk HaplotypeCaller \
+    -R ../02_alignment/reference/chr17.fa \
+    -I ../02_alignment/results/sorted/SRR062634_markdup.bam \
+    -O results/variants/germline/SRR062634_germline.vcf.gz \
+    --sample-name SRR062634 \
+    -L chr17:7661779-7687538
+
+# Count the number of variants
+bcftools view -H results/variants/germline/SRR062634_germline.vcf.gz | wc -l
+
+# Run the HaplotypeCaller that detects germline mutation. The tp53 region on the chromosome was also selected
+gatk Mutect2 \
+    -R ../02_alignment/reference/chr17.fa \
+    -I ../o2_alignment/results/sorted/SRR062634_markdup.bam \
+    -tumor SRR062634 \
+    -O results/variants/somatic/SRR062634_somatic.vcf.gz \
+    -L chr17:7661779-7687538 \
+    --af-of-alleles-not-in-resource 0.0000025
+
+# Count the number of variants
+bcftools view -H results/variants/somatic/SRR062634_somatic.vcf.gz | wc -l
+# Both results showed zero variants. So a different dataset was downloaded along with the full hg38 genome to be used as reference
+
+# Reference genome downloaded, unzipped, and indexed
+wget -P data/ https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz
+gunzip data/hg38.fa.gz 
+samtools faidx data/hg38.fa 
+samtools dict data/hg38.fa -o data/hg38.dict
+
+# Dataset to run variant calling on
+wget -P data/ https://42basepairs.com/download/s3/gatk-test-data/wgs_bam/NA12878_20k_hg38/NA12878.bam
+wget -P data/ https://42basepairs.com/download/s3/gatk-test-data/wgs_bam/NA12878_20k_hg38/NA12878.bai
+
+# Run the Haplotype Caller on the new dataset
+gatk HaplotypeCaller \
+    -R data/hg38.fa \
+    -I data/NA12878.bam \
+    -O results/variants/NA12878_germline.vcf.gz
+
+# Count the number of germline variants called
+bcftools view -H results/variants/NA12878_germline.vcf.gz | wc -l
+
+# Run the Mutect2 caller for somatic mutation and then filter using FilterMutectCalls
+gatk Mutect2 \
+     -R data/hg38.fa \
+     -I data/NA12878.bam \
+     -tumor NA12878 \
+     -O results/variants/NA12878_somatic.vcf.gz \
+     --af-of-alleles-not-in-resource 0.0000025
+
+gatk FilterMutectCalls \
+     -R data/hg38.fa \
+     -V results/variants/NA12878_somatic.vcf.gz \
+     -O results/variants/NA12878_somatic_filtered.vcf.gz
+
+# Count the number of somatic variations, unfiltered and filtered
+bcftools view -H results/variants/NA12878_somatic.vcf.gz | wc -l
+bcftools view -H results/variants/NA12878_somatic_filtered.vcf.gz | wc -l
+# None of the variants passed the Mutect2 calls so manual filtering will be done using other metrics
+
+
+
+
+
+
+
+
+
+
+
+
+
