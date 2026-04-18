eb8fc21967a0ddf6775b1659b2f3211fa76ea395
Sarmmie2706 <aladegbaiyesarmmie@gmail.com>
2026-04-18 15:44:37 +0100

    Annotate variants

--- 04-ngs_analysis/03_variants/scripts/variant_pipeline.sh
+++ 04-ngs_analysis/03_variants/scripts/variant_pipeline.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+# Commands that were not run but included in the script for the sake of understanding the pipeline are commented out
 set -eu
 
 # Run the HaplotypeCaller that detects germline mutation. The tp53 region on the chromosome was selected
@@ -35,6 +36,29 @@ samtools dict data/hg38.fa -o data/hg38.dict
 # Dataset to run variant calling on
 wget -P data/ https://42basepairs.com/download/s3/gatk-test-data/wgs_bam/NA12878_20k_hg38/NA12878.bam
 wget -P data/ https://42basepairs.com/download/s3/gatk-test-data/wgs_bam/NA12878_20k_hg38/NA12878.bai
+
+# BQSR corrects systematic errors in base quality scores using known variant sites. 
+# # Step 1 - Build recalibration table
+# Requires known sites VCF (dbSNP) - download first
+#wget https://storage.googleapis.com/genomics-public-data/resources/broad/hg38/v0/Homo_sapiens_assembly38.dbsnp138.vcf
+#wget https://storage.googleapis.com/genomics-public-data/resources/broad/hg38/v0/Homo_sapiens_assembly38.dbsnp138.vcf.idx
+
+#gatk BaseRecalibrator \
+#    -R reference/hg38.fa \
+#    -I results/sorted/NA12878_markdup.bam \
+#    --known-sites Homo_sapiens_assembly38.dbsnp138.vcf \
+#    -O results/sorted/NA12878_recal.table
+
+# Step 2 - Apply recalibration
+#gatk ApplyBQSR \
+#    -R reference/hg38.fa \
+#    -I results/sorted/NA12878_markdup.bam \
+#    --bqsr-recal-file results/sorted/NA12878_recal.table \
+#   -O results/sorted/NA12878_bqsr.bam
+
+# NOTE: BQSR not run in this analysis due to resource constraints
+# (dbSNP file ~10GB). Commands included for reference only.
+# In production, run BaseRecalibrator + ApplyBQSR before variant calling.
 
 # Run the Haplotype Caller on the new dataset
 gatk HaplotypeCaller \
@@ -96,7 +120,37 @@ grep "^TSTV" results/variants/NA12878_somatic_stats.txt
 bcftools norm -f data/hg38.fa -m -any results/variants/NA12878_somatic_filtered.vcf.gz -o results/variants/NA12878_somatic_normalised.vcf.gz -Oz
 bcftools view -H results/variants/NA12878_somatic_normalised.vcf.gz | wc -l
 
+# VEP is used for annotation. For this exercise, it was done directly on Ensembl's website for reasons explained in the report. But if it were to be done on the command line, these are the codes
+## For offline
+#vep \
+#    --input_file results/variants/somatic/SRR062634_normalised.vcf.gz \
+#    --output_file results/annotation/SRR062634_vep.vcf \
+#    --format vcf \
+#    --vcf \
+#    --symbol \
+#    --terms SO \
+#    --tsl \
+#   --hgvs \
+#    --fasta reference/chr17.fa \
+#    --offline \
+#    --cache \
+#    --dir_cache ~/.vep \
+#    --assembly GRCh38 \
+#    --pick
 
+## For online
+#vep \
+#    --input_file results/variants/somatic/SRR062634_normalised.vcf.gz \
+#    --output_file results/annotation/SRR062634_vep.vcf \
+#    --format vcf \
+#    --vcf \
+#    --symbol \
+#    --terms SO \
+#    --hgvs \
+#    --assembly GRCh38 \
+#    --pick \
+#    --database
 
+# The vep result from Ensembl was then compared with known cancer databases: ClinVar, COSMIC and gnomAD. The results are explained in the report.
 
 
